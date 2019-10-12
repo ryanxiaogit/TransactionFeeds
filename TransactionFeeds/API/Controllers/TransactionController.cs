1b28f284696aafcc876bae669581a40f4189aded
Ryan Xiao <ryanxiao@live.cn>
2019-10-12 22:49:47 +0800

    Add missing Api Project: endpoint of Service.

---
+++ TransactionFeeds/API/Controllers/TransactionController.cs
@@ -0,0 +1,13 @@
+﻿using API.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ServiceFilter(typeof(RequestValidation))]
+    public class TransactionController : ControllerBase
+    {
+
+    }
+}
---
+++ TransactionFeeds/API/Filters/RequestValidation.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Filters
+{
+    public class RequestValidation : ActionFilterAttribute
+    {
+    }
+}
